766fdb6bcd844c41ac01a08297dc07fc98d22be7
KedarnathSahu <kedarnath.sahu97@gmail.com>
2019-02-03 13:04:25 +0530

    first commit

---
+++ TestApp/index.js
@@ -0,0 +1,17 @@
+//Import Library to create component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+//Create component
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Album'} />
+        <AlbumList />
+    </View>
+);
+
+//Render it to the device
+AppRegistry.registerComponent('TestApp', () => App
+);
